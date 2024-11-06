a7a368aa4960bcf6164001d0e01d68a6ddda46c6
Jdexurbano <dexterurbano25@gmail.com>
2024-11-06 15:53:22 +0800

    add task serializer

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from core.models import Task
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password1 = serializers.CharField(write_only = True, required = True)
@@ -24,3 +25,9 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password1'])
         user.save()
         return user
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
