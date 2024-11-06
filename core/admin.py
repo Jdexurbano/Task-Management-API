d6be4855dbb0c80522411c7b273e33be5b632e68
Jdexurbano <dexterurbano25@gmail.com>
2024-11-06 15:34:00 +0800

    add task model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id','user','title','status')
 
 # Register your models here.
+admin.site.register(Task,TaskAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+#model for task
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ('pending','Pending'),
+        ('in_progress','In Progress'),
+        ('completed','Completed'),
+        ('on_hold','On Hold'),
+    ]
+
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'tasks')
+    title = models.CharField(max_length = 100)
+    description = models.TextField(max_length = 200)
+    status = models.CharField(max_length = 20, choices = STATUS_CHOICES, default = 'pending')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
